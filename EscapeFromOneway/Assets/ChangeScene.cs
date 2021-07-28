24adab4a0e4ba194b2a7107ac669d26b85de8bc3
PHS00 <88030920+PHS00@users.noreply.github.com>
2021-07-28 20:27:56 +0900

    Button수정/맵전환

--- EscapeFromOneway/Assets/ChangeScene.cs
+++ EscapeFromOneway/Assets/ChangeScene.cs
@@ -5,19 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public void ChangeSceneBtn()
+    public void ChangeSceneChallengeLobbyBtn()   //챌린지모드 로비화면
     {
         switch (this.gameObject.name)
         {
@@ -25,10 +14,157 @@ public class ChangeScene : MonoBehaviour
                 SceneManager.LoadScene("ModeChangeUI");
                 break;
             case "PlayButton":
-                SceneManager.LoadScene("IngameUI");
+                SceneManager.LoadScene("ChallengeIngameUI");
                 break;
             case "CharacterSelectButton":
                 SceneManager.LoadScene("CharacterSelectUI");
+                break;
+        }
+    }
+
+    public void ChangeSceneStageLobbyBtn()   //스테이지모드 로비화면
+    {
+        switch (this.gameObject.name)
+        {
+            case "ModeChangeButton":
+                SceneManager.LoadScene("ModeChangeUI");
+                break;
+            case "PlayButton":
+                SceneManager.LoadScene("StageSelectUI");
+                break;
+            case "CharacterSelectButton":
+                SceneManager.LoadScene("CharacterSelectUI");
+                break;
+        }
+    }
+
+    public void ChangeSceneChallengeResultBtn() //챌린지모드 결과화면
+    {
+        switch (this.gameObject.name)
+        {
+            case "RetryButton":
+                SceneManager.LoadScene("ChallengeIngameUI");
+                break;
+            case "HomeButton":
+                SceneManager.LoadScene("ChallengeLobbyUI");
+                break;
+        }
+    }
+
+    public void ChangeSceneStageResultBtn() //스테이지모드 결과화면
+    {
+        switch (this.gameObject.name)
+        {
+            case "RetryButton":
+                SceneManager.LoadScene("StageIngameUI");
+                break;
+            case "HomeButton":
+                SceneManager.LoadScene("StageLobbyUI");
+                break;
+            case "NextStageButton":
+                SceneManager.LoadScene("StageIngameUI");
+                break;
+        }
+    }
+
+    public void ChangeSceneChallengeIngameBtn()  //챌린지모드인게임 화면
+    {
+        switch (this.gameObject.name)
+        {
+            case "LeftButton":
+            
+                break;
+            case "UpButton":
+
+                break;
+            case "RightButton":
+                
+                break;
+            case "PauseButton":
+                SceneManager.LoadScene("ChallengePauseMenuUI");
+                break;
+        }
+    }
+
+    public void ChangeSceneStageIngameBtn()  //스테이지모드인게임 화면
+    {
+        switch (this.gameObject.name)
+        {
+            case "LeftButton":
+
+                break;
+            case "UpButton":
+
+                break;
+            case "RightButton":
+
+                break;
+            case "PauseButton":
+                SceneManager.LoadScene("StagePauseMenuUI");
+                break;
+        }
+    }
+
+    public void ChangeSceneChallengePauseMenuBtn()  //챌린지모드 일시정지메뉴
+    {
+        switch (this.gameObject.name)
+        {
+            case "PlayButton":
+                SceneManager.LoadScene("ChallengeIngameUI");
+                break;
+            case "HomeButton":
+                SceneManager.LoadScene("ChallengeLobbyUI");
+                break;
+            case "RetryButton":
+                SceneManager.LoadScene("ChallengeIngameUI");
+                break;
+        }
+    }
+
+    public void ChangeSceneStagePauseMenuBtn()  //스테이지모드 일시정지메뉴
+    {
+        switch (this.gameObject.name)
+        {
+            case "PlayButton":
+                SceneManager.LoadScene("StageIngameUI");
+                break;
+            case "HomeButton":
+                SceneManager.LoadScene("StageLobbyUI");
+                break;
+            case "RetryButton":
+                SceneManager.LoadScene("StageIngameUI");
+                break;
+            case "StageSelectButton":
+                SceneManager.LoadScene("StageSelectUI");
+                break;
+        }
+    }
+
+    public void ChangeSceneStageSelectBtn() //스테이지선택
+    {
+        switch (this.gameObject.name)
+        {
+            case "Stage1Button":
+                SceneManager.LoadScene("StageIngameUI");
+                break;
+            case "Stage2Button":
+                SceneManager.LoadScene("StageIngameUI");
+                break;
+            case "Stage3Button":
+                SceneManager.LoadScene("StageIngameUI");
+                break;
+        }
+    }
+
+    public void ChangeSceneModeChangeBtn()  //모드선택 화면
+    {
+        switch (this.gameObject.name)
+        {
+            case "StageModeButton":
+                SceneManager.LoadScene("StageLobbyUI");
+                break;
+            case "ChallengeModeButton":
+                SceneManager.LoadScene("ChallengeLobbyUI");
                 break;
         }
     }
