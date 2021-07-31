f1d7ee2af111ed2456cae348fe8c53baa89761c0
박현수 <hyunsoo159487@naver.com>
2021-07-31 12:12:16 +0900

    0731

--- EscapeFromOneway/Assets/Scripts/Controller/PlayerMove.cs
+++ EscapeFromOneway/Assets/Scripts/Controller/PlayerMove.cs
@@ -1,27 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//using static UnityEngine.SpriteRenderer;
+using System.Collections;
 
 public class PlayerMove : MonoBehaviour
 {
 
     Transform tr;
-
-    // SpriteRenderer sc = Transform.GetComponent<SpriteRenderer>();
-
-    //SpriteRenderer spriteR = gameObject.GetComponent<SpriteRenderer>();
-    //Sprite[] sprites = Resources.LoadAll<Sprite>("Sprites/Player/charcter_idle");
-    // SpriteRenderer spr = GetComponent<SpriteRenderer>();
-    //public GameObject player_left;
+    Animator animator;
 
     public float speed;
+    public Sprite Forward;
+    public Sprite Backward;
+    public Sprite Left;
 
     void Start()
     {
         tr = GetComponent<Transform>();
 
-
+        animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -30,24 +27,28 @@ public class PlayerMove : MonoBehaviour
         //float x = Input.GetAxis("Horizontal") * speed;
         //float y = Input.GetAxis("Vertical") * speed;
 
+       // bool L = false, U = false, D = false;
+
+       // animator.SetTrigger("Left");
+       // animator.SetTrigger("Forward");
+        //animator.SetTrigger("Backward");
+
+
         if (Input.GetKeyDown("left"))
         {
-            //for (; tr.position.x == tr.position.x + 3; tr.position.x =+ speed)
+            //L = Input.GetKeyDown("left");
+            animator.SetTrigger("Left");
+
             tr.position = new Vector2(tr.position.x - 3, tr.position.y);
-
-
-
-            //tr.position = new Vector2(tr.position.x - 3, tr.position.y);
-
-            //sc = (Sprite)Resources.Load("idle_21", typeof(Sprite));
-            //spriteR.sprite = sprites[21];
-            //spr.sprite = Resources.Load<Sprite>("Player/character_idle/idle_21");
-            //player_left.GetComponent<SpriteRenderer>().sprite = Resources.Load("idle_21", typeof(Sprite)) as Sprite;
 
         }
         if (Input.GetKeyDown("up"))
         {
+            // U = Input.GetKeyDown("up");
+            animator.SetTrigger("Forward");
+
             tr.position = new Vector2(tr.position.x, tr.position.y + 3);
+
         }
         if (Input.GetKeyDown("right"))
         {
@@ -55,10 +56,19 @@ public class PlayerMove : MonoBehaviour
         }
         if (Input.GetKeyDown("down"))
         {
+             //D = Input.GetKeyDown("down");
+            animator.SetTrigger("Backward");
             tr.position = new Vector2(tr.position.x, tr.position.y - 3);
+
         }
 
+    }
 
+    void animatorChange()
+    {
 
     }
+
+
+
 }
